9731bc196f5834c6c3198cd98e59777a839e5c8c
Dylan <liamsdk99@gmail.com>
2026-03-27 21:57:42 -0300

    sea: Moved Bluetooth audio HAL and libaedv to DT
    Moved HALs vendor.mediatek.hardware.bluetooth.audio@2.1 - 2.2 and libaedv to the device tree, no longer depending on arbitrary hardware/mediatek.

--- device.mk
+++ device.mk
@@ -352,7 +352,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
     hardware/mediatek \
-    hardware/mediatek/libaedv \
     hardware/mediatek/libmtkperf_client \
     hardware/xiaomi \
     hardware/google/pixel \
